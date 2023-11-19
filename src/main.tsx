4f815989d87b63d18c6e91b216afc3210a29f1c3
Irving Juárez <irving.juarez@thomsonreuters.com>
2023-11-19 12:54:58 -0600

    Error boundary refactor

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import App from './App.tsx'
 import './index.css'
 import router from './routing/router.tsx'
-import { ErrorBoundary } from 'react-error-boundary'
-import ErrorHandler from './components/ErrorHandler.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ErrorBoundary FallbackComponent={ErrorHandler}>
-      <RouterProvider router={router} />
-    </ErrorBoundary>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
